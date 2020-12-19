8b0dc6205f0b6a01f9c64df9b7d2d80f6c73f8c0
Luckymoi <samuel.buffet@gmail.com>
2020-12-19 18:22:21 +0100

    fixed plugin name for key registration during install

--- INSTALL.sh
+++ INSTALL.sh
@@ -8,6 +8,8 @@ echo "Installing pytorch dependencies..."
 sudo apt-get update
 sudo apt-get install python3-numpy
 
+echo "Installing yolo5 dependencies..."
+sudo pip3 install -r yolo5requirements.txt
 
 if [ ! -e "./conf.json" ]; then
     echo "Creating conf.json"
@@ -16,5 +18,5 @@ else
     echo "conf.json already exists..."
 fi
 echo "Adding Random Plugin Key to Main Configuration"
-node $DIR/../../tools/modifyConfigurationForPlugin.js pytorch key=$(head -c 64 < /dev/urandom | sha256sum | awk '{print substr($1,1,60)}')
+node $DIR/../../tools/modifyConfigurationForPlugin.js pytorchyolo5 key=$(head -c 64 < /dev/urandom | sha256sum | awk '{print substr($1,1,60)}')
 
