1f71ff0f65a95ba1bdbea3962ee332a6e6cb7571
Luckymoi <samuel.buffet@gmail.com>
2020-12-19 23:04:38 +0100

    added necessary variables for pytorch main detection function

--- detect_image.py
+++ detect_image.py
@@ -38,17 +38,6 @@ import detect
 import platform
 from datetime import datetime
 
-from pathlib import Path
-import torch
-import torch.backends.cudnn as cudnn
-from numpy import random
-
-from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized
-
 
 
                 
@@ -63,9 +52,20 @@ def printData(array, time):
 
 
 def main():
-#    labels = load_labels("models/coco_labels.txt")
-#    interpreter = make_interpreter("models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite")
-#    interpreter.allocate_tensors()
+
+    source = '/home/Shinobi/plugins/pytorchyolo5/image2.jpg'
+    weights =  ['yolov5s.pt']
+    view_img = False
+    save_img = True
+    save_txt = False
+    imgsz = 640
+    conf_thres = 0.4
+    iou_thres=0.45
+    classes=None
+    agnostic_nms=False
+    save_conf=False
+
+
     threshold = 0.4
     printInfo("ready")
     while True:
