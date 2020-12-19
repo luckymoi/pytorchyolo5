b4940fc37643f08733f84f96c46d8871aa93736a
Luckymoi <samuel.buffet@gmail.com>
2020-12-19 22:48:13 +0100

    all necessary utils added

--- detect_image.py
+++ detect_image.py
@@ -38,6 +38,17 @@ import detect
 import platform
 from datetime import datetime
 
+from pathlib import Path
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+
 
 
                 
