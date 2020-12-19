eb724e605816a4bdf3bb191671244c133d41f2de
Luckymoi <samuel.buffet@gmail.com>
2020-12-19 23:25:36 +0100

    weights are downloaded in the correct folder

--- INSTALL.sh
+++ INSTALL.sh
@@ -12,8 +12,9 @@ echo "Installing yolo5 dependencies..."
 sudo pip3 install -r yolo5requirements.txt
 
 echo "downloading yolo5 weights..."
-sudo cd weights
+cd weights
 sudo bash download_weights.sh
+cd ..
 
 if [ ! -e "./conf.json" ]; then
     echo "Creating conf.json"
