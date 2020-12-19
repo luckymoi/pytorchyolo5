09efb16e155075bd0ac57f0304cac1af56a0bc64
Luckymoi <samuel.buffet@gmail.com>
2020-12-20 00:00:07 +0100

    imports for pytorch not craching the script anymore

--- detect_image.py
+++ detect_image.py
@@ -38,6 +38,17 @@ import detect
 import platform
 from datetime import datetime
 
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+
 
 
                 
@@ -51,8 +62,7 @@ def printData(array, time):
     print(json.dumps({"type": "data", "data": array, "time": time}))
 
 
-def main():
-
+def detect():
     source = '/home/Shinobi/plugins/pytorchyolo5/image2.jpg'
     weights =  ['yolov5s.pt']
     view_img = False
@@ -107,4 +117,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    with torch.no_grad():
+      detect()
